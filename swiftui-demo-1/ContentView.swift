e60bf16103389b25a1c94681065e49c95cb4c296
TRINH TOAN TRUNG <trungtt@azgroup.co>
2019-10-14 15:37:56 +0700

    init project

---
+++ swiftui-demo-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-demo-1
+//
+//  Created by Trịnh Toàn Trung on 10/14/19.
+//  Copyright © 2019 Trịnh Toàn Trung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Trinh Toan Trung")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
